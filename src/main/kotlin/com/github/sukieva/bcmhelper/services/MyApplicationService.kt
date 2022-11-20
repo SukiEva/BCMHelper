f6e8199409177b9be2e4518eac66e2a593ae3650
GitHub Action <action@github.com>
2022-11-20 02:20:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/sukieva/bcmhelper/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/sukieva/bcmhelper/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.sukieva.bcmhelper.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sukieva.bcmhelper.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/sukieva/bcmhelper/services/MyProjectService.kt
+++ src/main/kotlin/com/github/sukieva/bcmhelper/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.sukieva.bcmhelper.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sukieva.bcmhelper.MyBundle
 
 class MyProjectService(project: Project) {
 
